de35e4ff48f9eff288984529ee8624e1b3ec5816
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2025-03-27 22:59:12 +0530

    Update challenge-6.py

--- list-basics/challenge-6.py
+++ list-basics/challenge-6.py
@@ -5,3 +5,12 @@ For example, for [1, 2, 3], the expected output is [3, 2, 1].
 
 Don't use the reverse list builtin function!
 """
+
+def reverse(lst):
+    res = []
+    for i in range(len(lst) - 1, -1, -1):
+        res.append(lst[i])
+    return res
+
+# Output
+print(reverse([1, 2,3]))
