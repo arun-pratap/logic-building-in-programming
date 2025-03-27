2bf4222013e5ec04da8981a594474344b8aa42c2
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2025-03-27 19:41:07 +0530

    Create challenge-3.py

---
+++ functions/challenge-3.py
@@ -0,0 +1,12 @@
+"""
+Create a function called square_number that takes a single parameter n and returns its square. 
+Then, call the function with the input value and store the result in a variable called result. 
+Finally, print the value of result.
+"""
+
+def square_number(n):
+    return n * n
+
+result = square_number(int(input()))
+
+print(result)
