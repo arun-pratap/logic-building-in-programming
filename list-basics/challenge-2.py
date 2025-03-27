0796044ebadbea752ba5b4b7786b80bbeb8925b0
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2025-03-27 22:14:06 +0530

    Create challenge-2.py

---
+++ list-basics/challenge-2.py
@@ -0,0 +1,10 @@
+"""
+Create a function named values that receives a list as an argument and prints all of the items in the list one after the other.
+
+To iterate over a list use the len() function inside the range() function:
+
+my_list = [10, 20, 30, 40, 50]
+for i in range(len(my_list)):
+    my_list[i] 
+This way, i will iterate from 0 to len(my_list) (not including) which is exactly all of the list indices.
+"""
