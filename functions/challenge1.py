408bd5eefec6643ef4a5cee08aa9cab7727834cf
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2025-03-27 18:24:55 +0530

    Update challenge1.py

--- functions/challenge1.py
+++ functions/challenge1.py
@@ -1,1 +1,20 @@
+"""
+Write a program that gets one input, a number. The input number indicates how many times to execute the below function. 
 
+Create a function that calculates the sum of all of the numbers between 1 and 10000 (including) and prints it, name the function however you like.
+
+Note! In your code, write the function before it's call statements.
+"""
+
+
+def calculate_sum():
+    sum = 0
+    for num in range(1, 10001):
+        sum += num
+    print(sum)
+
+
+n = int(input())
+for n in range(n):
+    calculate_sum()
+    
