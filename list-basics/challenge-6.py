3f35ac97b2fc10e9183288e969d302add467ed6c
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2025-03-27 22:49:27 +0530

    Create challenge-6.py

---
+++ list-basics/challenge-6.py
@@ -0,0 +1,7 @@
+"""
+Write a function named reverse which gets a list of numbers as argument and returns the reversed list.
+
+For example, for [1, 2, 3], the expected output is [3, 2, 1].
+
+Don't use the reverse list builtin function!
+"""
