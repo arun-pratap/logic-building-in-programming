c1f6e86fc2fbc5dd1e44e59a462e50f2182e79af
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2025-03-28 13:14:32 +0530

    Update challenge-2.py

--- Iterating over a sequence/challenge-2.py
+++ Iterating over a sequence/challenge-2.py
@@ -2,3 +2,13 @@
 Write a program that receives a list of numbers as input (given),
 and prints a list of the indices of the numbers that are either below 50 or they are divisible by 5. 
 """
+
+lst = list(map(int, input().split(",")))
+# Write your code below
+
+res  = []
+for i in range(len(lst)):
+    if (lst[i] < 50 or lst[i] % 5 == 0):
+        res.append(i)
+
+print(res)
