a7a79bb36d5bc2d9453b61539d87d2993232392a
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2025-03-28 11:36:36 +0530

    Update fizzbuzz.py

--- list-basics/fizzbuzz.py
+++ list-basics/fizzbuzz.py
@@ -10,3 +10,38 @@ def fizzbuzz(n):
     return str(n)
 
 print(fizzbuzz(int(input())))
+
+"""
+Looping The Numbers
+Everything is ready, let's play the game!
+
+Challenge:
+Loop over the numbers from 1 to the number that the user entered, and each time use the function you created to calculate the FizzBuzz result and output it.
+For example, for input of 7 output:
+Welcome to FizzBuzz!
+1
+2
+Fizz
+4
+5
+Fizz
+Buzz
+"""
+
+print("Welcome to FizzBuzz!")
+
+def fizzbuzz(n):
+    if  n % 3 == 0 and n % 7 == 0:
+        return "FizzBuzz"
+    if n % 3 == 0:
+        return "Fizz"
+    if n % 7 == 0:
+        return "Buzz"
+    return str(n)
+
+no = int(input())
+for no in range(1, no+1):
+    print(fizzbuzz(no))
+
+
+# print(fizzbuzz(int(input())))
